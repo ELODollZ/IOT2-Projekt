e3f8c3891cd68c7b95e5cfceb47f05ee88e87765
ELODollZ <DanielDKN29@gmail.com>
2023-03-27 11:41:28 +0200

    MeasureScript

--- ESP32BackUp/main.py
+++ ESP32BackUp/main.py
@@ -2,9 +2,8 @@
 ###Main File
 ### Imports
 import sys
-import graphFile
-import indexFile
-import CreditInformation
+#import graphFile
+#import indexFile
 import InternetConnector
 import measure
 ### StartUps
@@ -14,11 +13,40 @@ measure.measuredData()
 ### Variables
 
 
+### Main Functions
+def sub_cb(topic, msg):
+    print((topic, msg))
+    if topic == b'Notification' and msg == b'received':
+        print('ESP received hello message')
+
+def connect_and_subscribe():
+    global client_id, mqtt_server, topic_sub
+    client = MQTTClient(client_id, mqtt_server)
+    client.set_callback(sub_cb)
+    client.connect()
+    client.subscribe(topic_sub)
+    print('Connected to %s MQTT broker, subribed to %s topic' % (mqtt_server, topic_sub))
+    return client
+def restart_and_reconnect():
+    print('Failed to connect to MQTT broker. Reconnecting...')
+    time.sleep(10)
+    machine.reset()
+try:
+   client = connect_and_subscribe()
+except OSError as e:
+   restart_and_reconnect()
 
 ### Main Loop
 while True():
     try:
-        
+        client.check_msg()
+        if (time.time() - last_message) > message_interval:
+            msg = b'measuredData #%d' % counter
+            client.publish(topic_pub, msg)
+            last_message = time.time()
+            counter += 1
+    except OSError as e:
+        restart_and_reconnect()
 
     except TypeError:
         print("Type error somewhere in code, functional")
