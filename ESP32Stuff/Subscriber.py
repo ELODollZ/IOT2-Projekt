eb0e49c2b768dcda9cf043b2a2b91424ec00eb34
ELODollZ <DanielDKN29@gmail.com>
2023-03-27 17:50:32 +0200

    RPIFIXV10

--- ESP32Stuff/Subscriber.py
+++ ESP32Stuff/Subscriber.py
@@ -29,7 +29,7 @@ def connect_mqtt() -> mqtt_client:
     client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
     client.on_connect = on_connect
-    client.connect(broker, port)
+    client.connect(broker, mqtt_server)
     return client
 
 
