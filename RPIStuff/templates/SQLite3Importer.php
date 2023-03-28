4fe7dfef0e966811603602d33d70aa2c91f8cd3d
ELODollZ <DanielDKN29@gmail.com>
2023-03-28 12:11:56 +0200

    PHP

---
+++ RPIStuff/templates/SQLite3Importer.php
@@ -0,0 +1,28 @@
+<?php
+	class MeasuredData extends SQLite3 {
+		function __construct(){
+		$this->open('measuredData.db')
+		}
+	}
+	$db = new MeasuredData();
+	if(!$db) {
+		echo $db->lastErrorMsg();
+	}
+	else {
+		echo "Open database successfully\n"
+	}
+
+   $sql =<<<EOF
+      INSERT INTO measuredData(ID, TEMP, HUMD, SMOKE, XLOCATION, YLOCATION);
+	VALUES ([ID], [TEMP], [HUMD], [SMOKE], [XLOCATION], [YLOCATION])
+EOF;
+
+   $ret = $db->exec($sql);
+   if(!$ret) {
+	   echo $db->lastErrorMsg();
+   }
+   else {
+	   echo "Records created successfully\n";
+   }
+   $db->close();
+?>
