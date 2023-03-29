a41dc2d8ca6769b326526bbf831e3d3c44eb95ac
ELODollZ <DanielDKN29@gmail.com>
2023-03-29 13:01:25 +0200

    TESTING

--- RPIBackup/SubScriber.sh
+++ RPIBackup/SubScriber.sh
@@ -2,12 +2,24 @@
 
 ###Variables
 messages=""
+pastmessages=""
 SentMessages="Messages sent"
+
 ###Functions
 while :
 do
+echo pastmessages
 mosquitto_sub	-h	localhost	-t	"ESP32Data"
 read messages
 echo messages
+IFS=',' read -r -a array <<< messages
+$TEMP = array[0]
+$HUMD = array[1]
+$SMOKE = array[2]
+$XLOCATION = array[3]
+$YLOCATION = array[4]
+pastmessages = messages
+echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION "IS IT WORKING!"
+sqlite3 measuredData.db "INSERT INTO measuredData(TEMP, HUMD, SMOKE, XLOCATION, YLOCATION) VALUES ($TEMP,$HUMD, "$SMOKE", $XLOCATION, $YLOCATION);"
 done
 
