e8ac99635c74178d5a4554f904b45c35d799539f
ELODollZ <DanielDKN29@gmail.com>
2023-03-30 22:52:53 +0200

    Subscriber done

---
+++ RPIBackup/TestSubScrib.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###Variables
+messages=""
+pastmessages=""
+testmessages="123.4, 123.4, False, 123.4, 123.4"
+SentMessages="Messages sent"
+TEMP=
+HUMD=
+SMOKE=
+XLOCATION=
+YLOCATION=
+###Functions
+while :
+do
+mosquitto_sub	-h	localhost	-t	"ESP32Data"
+read $messages
+IFS=',' read -r TEMP HUMD SMOKE XLOCATION YLOCATION <<<"${messages}"
+	if [[ "$pastmessages" == "$messages" ]]
+	echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION
+	Database= ~/IOT2-Projekt/DataBase/measuredData.db
+	if [[ ! -r "$DataBase" ]]
+		sqlite3 $DataBase "INSERT INTO measuredData(TEMP, HUMD, SMOKE, XLOCATION, YLOCATION) VALUES ($TEMP, $HUMD, "$SMOKE", $XLOCATION, $YLOCATION);"
+	then
+		echo No DataBase
+	fi
+then
+echo $SentMessages
+fi
+done
+
