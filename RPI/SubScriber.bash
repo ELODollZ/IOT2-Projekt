b89c8be15e96055bfdad86d14ff0b91449e0489b
ELODollZ <DanielDKN29@gmail.com>
2023-03-31 20:38:21 +0200

    FIRST INSERT LETS GOO!

--- RPI/SubScriber.bash
+++ RPI/SubScriber.bash
@@ -11,22 +11,23 @@ XLOCATION=
 YLOCATION=
 DataBase="measuredData.db"
 ###Functions
-function reader {
+function readerFunction() {
 	mosquitto_sub	-h	localhost	-t	"EPS32Data"
 	read $messages
+		
 	return $messages
 }
 while :
 do
-	mosquitto_sub	-h	localhost	-t	"ESP32Data"
-	read $messages
-	#reader 
+	#mosquitto_sub	-h	localhost	-t	"ESP32Data"
+	#read $messages
+	readerFunction 
 	IFS=',' read -r TEMP HUMD SMOKE XLOCATION YLOCATION <<<"${messages}"
 	if [[ "$pastmessages" == "$messages" ]]; then
 		echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION
 		if [[ measuredData.db == $DataBase ]]; then
 			echo "trying to insert into SQLite3"
-			sqlite3 $DataBase "insert into measuredData(TEMP,HUMD,SMOKE,XLOCATION,YLOCATION) values $($TEMP $HUMD '$SMOKE' $XLOCATION $YLOCATION)"
+			sqlite3 $DataBase "INSERT INTO measuredData(TEMP,HUMD,SMOKE,XLOCATION,YLOCATION) values ($TEMP $HUMD '$SMOKE' $XLOCATION $YLOCATION)"
 			echo "inserted into Database"
 		else
 			echo "No DataBase found"
