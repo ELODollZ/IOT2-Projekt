16a0e0095f8b6e46665a54693837b0c69813d2b7
ELODollZ <DanielDKN29@gmail.com>
2023-03-31 23:30:50 +0200

    First column inserted automated

--- RPI/test.sh
+++ RPI/test.sh
@@ -10,13 +10,14 @@ SMOKE=
 XLOCATION=
 YLOCATION=
 DataBase="measuredData.db"
+IFS=","
 ###Functions
 while :
 do
-mosquitto_sub	-h	localhost	-t	"ESP32Data"
-read $messages
-IFS=',' read -r TEMP HUMD SMOKE XLOCATION YLOCATION <<<"${messages}"
-echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION
+mosquitto_sub	-h	localhost	-t	"ESP32Data" -C	1	-R	$messages
 echo $messages
-sqlite3 measuredData.db "INSERT INTO measuredData(TEMP,HUMD,SMOKE,XLOCATION,YLOCATION) VALUES ($TEMP, $HUMD, '$SMOKE', $XLOCATION, $YLOCATION)"
+read -r TEMP HUMD SMOKE XLOCATION YLOCATION <<<"${messages}"
+echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION
+sqlite3 measuredData.db "INSERT INTO measuredData(TEMP,HUMD,SMOKE,XLOCATION,YLOCATION) VALUES ($TEMP $HUMD '$SMOKE' $XLOCATION $YLOCATION)"
+echo $SentMessages
 done
