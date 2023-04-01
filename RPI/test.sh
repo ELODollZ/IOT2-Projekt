36df511fb73dda270c2488d507e1bbf08bf4830d
ELODollZ <DanielDKN29@gmail.com>
2023-04-01 11:20:19 +0200

    syntax error somewhere

--- RPI/test.sh
+++ RPI/test.sh
@@ -9,15 +9,14 @@ HUMD=
 SMOKE=
 XLOCATION=
 YLOCATION=
-DataBase="measuredData.db"
-IFS=","
+DataBase="/home/NyboMønster/IOT2-Projekt/RPI/measuredData.db"
+IFS=','
 ###Functions
 while :
 do
 mosquitto_sub	-h	localhost	-t	"ESP32Data" -C	1	-R	$messages
-echo $messages
 read -r TEMP HUMD SMOKE XLOCATION YLOCATION <<<"${messages}"
-#echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION
-sqlite3 measuredData.db "INSERT INTO measuredData(TEMP,HUMD,SMOKE,XLOCATION,YLOCATION) VALUES ($TEMP,$HUMD,'$SMOKE',$XLOCATION,$YLOCATION)"
+echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION
+sqlite3 /home/NyboMønster/IOT2-Projekt/RPI/measured.db "INSERT INTO measuredData(TEMP,HUMD,SMOKE,XLOCATION,YLOCATION) VALUES ($TEMP,$HUMD,$SMOKE,$XLOCATION,$YLOCATION);"
 echo $SentMessages
 done
