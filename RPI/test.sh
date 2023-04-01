447753f8b50177efdb3d35c625b1d359e28dcecc
ELODollZ <DanielDKN29@gmail.com>
2023-04-01 10:57:08 +0200

    ShouldWorkAutomated

--- RPI/test.sh
+++ RPI/test.sh
@@ -17,7 +17,7 @@ do
 mosquitto_sub	-h	localhost	-t	"ESP32Data" -C	1	-R	$messages
 echo $messages
 read -r TEMP HUMD SMOKE XLOCATION YLOCATION <<<"${messages}"
-echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION
-sqlite3 measuredData.db "INSERT INTO measuredData(TEMP,HUMD,SMOKE,XLOCATION,YLOCATION) VALUES ($TEMP $HUMD '$SMOKE' $XLOCATION $YLOCATION)"
+#echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION
+sqlite3 measuredData.db "INSERT INTO measuredData(TEMP,HUMD,SMOKE,XLOCATION,YLOCATION) VALUES ($TEMP,$HUMD,'$SMOKE',$XLOCATION,$YLOCATION)"
 echo $SentMessages
 done
