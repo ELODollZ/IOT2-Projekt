eb2d6c6f41161f30b050ba913570adf243207350
ELODollZ <DanielDKN29@gmail.com>
2023-04-01 13:16:52 +0200

    folder

---
+++ RPI/FinalSubScriber.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+###Variables
+messages=""
+pastmessages=""
+testmessages="123.4, 123.4, False, 123.4, 123.4"
+SentMessages="Messages sent"
+TEMP=
+HUMD=
+SMOKE=
+XLOCATION=
+YLOCATION=
+DataBase="/home/NyboMønster/IOT2-Projekt/RPI/measuredData.db"
+IFS=','
+###Functions
+while :
+do
+mosquitto_sub	-h	localhost	-t	"ESP32Data" -C	1	-R	$messages
+read -r TEMP HUMD SMOKE XLOCATION YLOCATION <<<"${messages}"
+echo $TEMP $HUMD $SMOKE $XLOCATION $YLOCATION
+sqlite3 $DataBase "INSERT INTO measuredData(TEMP,HUMD,SMOKE,XLOCATION,YLOCATION) VALUES ($TEMP,$HUMD,$SMOKE,$XLOCATION,$YLOCATION);"
+echo $SentMessages
+done
+
